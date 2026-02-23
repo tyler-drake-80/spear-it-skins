477000a9557968c8ec618f54ba86c8bed4526f93
manny785 <emanuel344742@gmail.com>
2026-02-23 00:47:59 -0500

    added image prototype

--- FrontEnd/my-app/src/pages/homePage.jsx
+++ FrontEnd/my-app/src/pages/homePage.jsx
@@ -1,11 +1,20 @@
-
+import {Link} from "react-router-dom";
+import DesertEagle from "../DesertEagle.jpg"
 function HomePage() {
   return (
     <>
 
 
       <h1>Home Page </h1>
-      <h3>will add gun pcitures w/ links to seperate categories</h3>
+      <h3>will add more gun pcitures w/ links to seperate categories</h3>
+      <h3>also will add a home button</h3>
+
+      <Link to = {"/category/pistols"}>
+        
+          <img src={DesertEagle} alt = "Pistols" style = {{width: "200px"}}/> 
+          <h2>pistols</h2>
+      
+      </Link>
     </>
   );
 }
